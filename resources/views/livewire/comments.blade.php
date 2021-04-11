f4620807c87a35708e30f75ae5a3eb3767ea0ca6
TafhimFaisal <tropical10tafhim@gmail.com>
2021-04-11 23:37:38 +0600

    comments

---
+++ resources/views/livewire/comments.blade.php
@@ -0,0 +1,23 @@
+<div class="container p-5">
+    <form>
+        <div class="form-group row">
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+          </div>
+          <div class="form-group col-sm-3">
+            <div class="col-sm-10">
+              <button type="submit" class="btn btn-primary">Add</button>
+            </div>
+          </div>
+        </div>
+    </form>
+    <div class="card mt-2" style="width: 75%;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Edit</a>
+          <a href="#" class="card-link">Delete</a>
+        </div>
+    </div>
+</div>
