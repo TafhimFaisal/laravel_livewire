2689f9c67e068f950346104c249ce20e4c23d276
TafhimFaisal <tropical10tafhim@gmail.com>
2021-04-11 23:07:56 +0600

    counter

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $counter = 0;
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+
+    public function increament()
+    {
+        $this->counter++;
+    }
+
+    public function decreament()
+    {
+        $this->counter--;
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div style="text-align: center">
+   <button wire:click="increament">+</button>
+   <p>{{$counter}}</p>
+   <button wire:click="decreament">-</button>
+</div>
