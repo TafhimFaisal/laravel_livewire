703728658e4b91981beb4f6ad3c679bc9e4737bc
TafhimFaisal <tropical10tafhim@gmail.com>
2021-04-17 13:19:54 +0600

    remove

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Comment::factory(20)->create();
+        // \App\Models\User::factory(10)->create();
+        // \App\Models\Comment::factory(20)->create();
+        // \App\Models\Book::factory(100)->create();
+        // \App\Models\Chapter::factory(300)->create();
+        // \App\Models\Section::factory(1000)->create();
+        \App\Models\Image::factory(500)->create();
     }
 }
